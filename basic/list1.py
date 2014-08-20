bc4c6adfe4fb219d7cb8c4ae139eb0ca93b329fc
Tim MacArthur <tim.macarthur@windsorcircle.com>
2014-08-20 16:53:31 -0400

    finished the sort exercises in list1.py

--- basic/list1.py
+++ basic/list1.py
@@ -36,8 +36,10 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-  # +++your code here+++
-  return
+	words.sort()
+	hasx = [word + '' for word in words if word[0]=='x']
+	nox = [word + '' for word in words if word[0]!='x']
+	return hasx + nox
 
 
 
@@ -47,9 +49,12 @@ def front_x(words):
 # e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
+def last_elem(tuple):
+	return tuple[-1]
+
 def sort_last(tuples):
-  # +++your code here+++
-  return
+	answers = sorted(tuples, key=last_elem)
+	return answers
 
 
 # Simple provided test() function used in main() to print
